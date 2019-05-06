00a57a1b27e4879653bf2ed4e321b29902fe9464
Jack Murphy <jackmurphy569@gmail.com>
2019-05-06 12:36:32 -0500

    Wrapped ML into API

---
+++ MLserver/API.py
@@ -0,0 +1,25 @@
+from flask import Flask, g, request
+from flask_restful import Resource,  Api, reqparse
+import rankPhotos
+import markdown
+import os
+import shelve
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/', methods = ['POST', 'GET'])
+def home():
+    filepath = ''
+
+    if 'filepath' in request.form:
+        print('filepath is found')
+        filepath = str(request.form.get('filepath'))
+        print(filepath)
+        return str(rankPhotos.rankData(filepath))
+        print('finished api call')
+    else:
+        print('no filepath')
+        return "bad filepath"
+
+app.run()
--- MLserver/rankPhotos.py
+++ MLserver/rankPhotos.py
@@ -10,14 +10,19 @@ import numpy as np
 
 def rankData(inputfilepath):
     #runs ML model against the dataset of photos
+    print('started rank photos')
     rankingModel = modelLoad.loadranking()
+    print('ranking model loaded')
     inputImageArray = normalizePictures.normalizepicture(inputfilepath)
+    print('image normalized')
     predictions = rankingModel.predict(inputImageArray)
-    
+    print('ml model has run')
     ranking = np.argmax(predictions[0])
+    print('got predition')
+    print(type(ranking))
+    print(ranking.item())
+    print(type(ranking.item()))
 
-
-
-    return ranking
+    return ranking.item()
 
 
